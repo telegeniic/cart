7a79c236d35b1764aca06a6a1442045eb2a37c42
Carlos Emilio <carlos.emilio99@hotmail.com>
2022-04-30 16:59:05 -0500

    searchboard animado

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../api/products.service';
+import { QuotationService } from '../api/quotation.service';
+import { StorageService } from '../api/storage.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  listas: any [] = [];
+  constructor(private storage: StorageService, private products: ProductsService) { 
+    storage.productsListObservable.subscribe(data =>{
+     this.listas = data
+    })
+    products.getAllProducts()
   }
+  
+  ngOnInit() {
+
+  }
+
+  buscar(event){
+    console.log(event);
+  }
+
 
 }
